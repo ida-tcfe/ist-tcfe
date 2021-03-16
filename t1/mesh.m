a1b0ce34c327874750cca466a7eb410c0d6c6ca9
084intheImpala <camoessousa@gmail.com>
2021-03-16 12:05:19 +0000

    ana

--- t1/mesh.m
+++ t1/mesh.m
@@ -14,4 +14,23 @@ A = [1,0,0,0;0,1-Kb*R3,Kb*R3,0;0,-R3,R1+R3+R4,R4;0,0,R4,R4+R6+R7-Kc]
 
 b = [Id;0;-Va;0]
 
-A\b
+c = A\b
+
+# same as gamma
+V1 = R1 * c(3)
+# same as beta
+V2 = R2 * c(2)
+# same as beta, back for gamma
+V3 = R3 * (c(2)-c(3))
+# same as gamma and delta
+V4 = R4 * (c(3)+c(4))
+# same as beta, back for alpha
+V5 = R5 * (c(2)-c(1))
+# same as delta
+V6 = R6 * c(4)
+# same as delta
+V7 = R7 * c(4)
+
+Ib = Kb * V3
+
+Vc = - Kc * c(4)
