4b4f8373eef963996db4349b0f7532a7307e84fd
IAlex19 <isabelmalexandre@tecnico.ulisboa.pt>
2021-04-01 17:19:41 +0100

    node analysis

--- t2/mat/Sim_data.m
+++ t2/mat/Sim_data.m
@@ -33,17 +33,25 @@ Kd = str2double(wval{11})*1000
 
 %%%%%%%%%% Escrita do ficheiro
 
-fprintf(fid, ".param R1val = %sk \n", wval{1} ) 
-fprintf(fid, ".param R2val = %sk \n", wval{2}) 
-fprintf(fid, ".param R3val = %sk \n", wval{1,3}) 
-fprintf(fid, ".param R4val = %sk \n", wval{1,4}) 
-fprintf(fid, ".param R5val = %sk \n", wval{1,5}) 
-fprintf(fid, ".param R6val = %sk \n", wval{1,6}) 
-fprintf(fid, ".param R7val = %sk \n", wval{1,7}) 
-fprintf(fid, ".param Vsval = %s \n", wval{1,8}) 
-fprintf(fid, ".param Cval = %suF \n", wval{1,9}) 
-fprintf(fid, ".param Kbval = %smS \n", wval{1,10}) 
-fprintf(fid, ".param Kdval = %sk \n", wval{1,11}) 
+fprintf(fid, ".param R1val = %sk \n", wval{1}); 
+fprintf(fid, ".param R2val = %sk \n", wval{2});
+fprintf(fid, ".param R3val = %sk \n", wval{1,3}); 
+fprintf(fid, ".param R4val = %sk \n", wval{1,4});
+fprintf(fid, ".param R5val = %sk \n", wval{1,5}); 
+fprintf(fid, ".param R6val = %sk \n", wval{1,6});
+fprintf(fid, ".param R7val = %sk \n", wval{1,7}); 
+fprintf(fid, ".param Vsval = %s \n", wval{1,8});
+fprintf(fid, ".param Cval = %suF \n", wval{1,9}); 
+fprintf(fid, ".param Kbval = %smS \n", wval{1,10}); 
+fprintf(fid, ".param Kdval = %sk \n", wval{1,11});
+
+# Node analysis t<0
+
+A = [1,0,0,0,0,0,0;-1/R1,1/R1,0,1/R4,0,1/R6,0;1/R1,-1/R1-1/R3-1/R2,1/R2,1/R3,0,0,0;0,1/R2+Kb,-1/R2,-Kb,0,0,0;0,-Kb,0,Kb+1/R5,-1/R5,0,0;0,0,0,0,0,-1/R6-1/R7,1/R7;0,0,0,1,0,Kd/R6,-1];
+
+b = [Vs;0;0;0;0;0;0];
+
+c = A\b;
 
 #printf("---- Voltages ----\n");
 #printf("V1 = %f\n", c(1));
@@ -54,4 +62,54 @@ fprintf(fid, ".param Kdval = %sk \n", wval{1,11})
 #printf("V7 = %f\n", c(6));
 #printf("V8 = %f\n", c(7));
 
+fnode = fopen("node.tex", "a");
+
+fprintf(fnode, "\\hline\n");
+fprintf(fnode, "$V_1\\;(V)$ & $%f$ \\\\ \n", c(1)); #V1
+fprintf(fnode, "\\hline\n");
+fprintf(fnode, "$V_2\\;(V)$ & $%f$ \\\\ \n", c(2)); #V2
+fprintf(fnode, "\\hline\n");
+fprintf(fnode, "$V_3\\;(V)$ & $%f$ \\\\ \n", c(3)); #V3
+fprintf(fnode, "\\hline\n");
+fprintf(fnode, "$V_5\\;(V)$ & $%f$ \\\\ \n", c(4)); #V5
+fprintf(fnode, "\\hline\n");
+fprintf(fnode, "$V_6\\;(V)$ & $%f$ \\\\ \n", c(5)); #V6
+fprintf(fnode, "\\hline\n");
+fprintf(fnode, "$V_7\\;(V)$ & $%f$ \\\\ \n", c(6)); #V7
+fprintf(fnode, "\\hline\n");
+fprintf(fnode, "$V_8\\;(V)$ & $%f$ \\\\ \n", c(7)); #V8
+
+I1 = (c(2)-c(1)) / R1;
+I2 = (c(3)-c(2)) / R2;
+Vb = c(2)-c(4);
+I3 = Vb / R3;
+I4 = c(4) / R4;
+I5 = (c(5)-c(4)) / R5;
+Id = -c(6) / R6;
+I7 = (c(7)-c(6)) / R7;
+Ib = Kb*Vb;
+IVd = I5-I4+I3;
+
+fprintf(fnode, "\\hline\n");
+fprintf(fnode, "$I_1\\;(mA)$ & $%f$ \\\\ \n", I1*1000);
+fprintf(fnode, "\\hline\n");
+fprintf(fnode, "$I_2\\;(mA)$ & $%f$ \\\\ \n", I2*1000);
+fprintf(fnode, "\\hline\n");
+fprintf(fnode, "$I_3\\;(mA)$ & $%f$ \\\\ \n", I3*1000);
+fprintf(fnode, "\\hline\n");
+fprintf(fnode, "$I_4\\;(mA)$ & $%f$ \\\\ \n", I4*1000);
+fprintf(fnode, "\\hline\n");
+fprintf(fnode, "$I_5\\;(mA)$ & $%f$ \\\\ \n", I5*1000);
+fprintf(fnode, "\\hline\n");
+fprintf(fnode, "$I_6\\;(mA)$ & $%f$ \\\\ \n", Id*1000);
+fprintf(fnode, "\\hline\n");
+fprintf(fnode, "$I_7\\;(mA)$ & $%f$ \\\\ \n", I7*1000);
+fprintf(fnode, "\\hline\n");
+fprintf(fnode, "$I_b\\;(mA)$ & $%f$ \\\\ \n", Ib*1000);
+fprintf(fnode, "\\hline\n");
+fprintf(fnode, "$IV_s\\;(mA)$ & $%f$ \\\\ \n", I1*1000);
+fprintf(fnode, "\\hline\n");
+fprintf(fnode, "$IV_d\\;(mA)$ & $%f$ \\\\ \n", IVd*1000);
+
+fclose(fnode)
 fclose(fid)
